524d860a9b62c8a6226ff1da73316143b5fde008
Anikeath <ekka3118@gmail.com>
2025-08-06 14:50:13 +0530

    code updated

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+
 class Search extends StatelessWidget {
   const Search({super.key});
 
   @override
   SafeArea build(BuildContext context) {
-    return  SafeArea(
-      child: Text('search', style: TextStyle(fontSize: 50), selectionColor: Colors.black, ));
+    return SafeArea(
+      child: Scaffold(
+        body: Text(
+          'search',
+          style: TextStyle(fontSize: 50),
+          selectionColor: Colors.black,
+        ),
+      ),
+    );
   }
 }
